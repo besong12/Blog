cb86dac07c4d5ab2eb162cd06fc3b8fbcbb2777c
besonghannah516 <besonghannah516@gmail.com>
2019-05-02 23:49:41 -0400

    fixing migration issue

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_194139) do
+ActiveRecord::Schema.define(version: 2019_04_24_050307) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-    t.integer "user_id"
     t.text "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
